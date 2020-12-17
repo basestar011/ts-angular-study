fd02e1f7d8f87c2b294a3bd3dc28089e9e7a8783
dongjun.han <dongjun.han@owaysoft.com>
2020-12-17 14:12:00 +0900

    20-12-17

--- src/20-12-16/1.Basic-Types.js
+++ src/20-12-16/1.Basic-Types.js
@@ -1,29 +1,30 @@
 // 타입스크립트의 데이터 타입 : 자바스크립트와 거의 동일한 데이터 타입 지원
 // 데이터 타입 종류
 // 1) Boolean
-var isDone = false;
+let isDone = false;
 // 2) Number
-var decimal = 6;
-var hex = 0xf00d;
-var binary = 10;
-var octal = 484;
+let decimal = 6;
+let hex = 0xf00d;
+let binary = 0b1010;
+let octal = 0o744;
 // 3) String
-var color = "blue";
+let color = "blue";
 color = 'red';
 // - 템플릿 문자열 사용
-var fullName = "Bob Bobbington";
-var age = 37;
-var sentence1 = "Hello, my name is " + fullName + ".\nI'll be " + (age + 1) + " years old next month.";
+let fullName = `Bob Bobbington`;
+let age = 37;
+let sentence1 = `Hello, my name is ${fullName}.
+I'll be ${age + 1} years old next month.`;
 // - 일반 문자열 사용
-var sentence2 = "Hello, my name is " + fullName + ".\n\n" + "I'll be " + (age + 1) + " years old next month.";
+let sentence2 = "Hello, my name is " + fullName + ".\n\n" + "I'll be " + (age + 1) + " years old next month.";
 // 4) Array
 // - 배열 타입 뒤에 [] 사용
-var list1 = [1, 2, 3];
+let list1 = [1, 2, 3];
 // - 제네릭 배열 타입 사용
-var list2 = [1, 2, 3];
+let list2 = [1, 2, 3];
 // 5) Tuple - 크기와 각 요소의 타입을 고정한 배열을 표현
 // tuple 타입으로 선언
-var x;
+let x;
 // 값 초기화
 x = ["hello", 10];
 // 잘못된 초기화
@@ -39,7 +40,7 @@ var Color1;
     Color1[Color1["Green"] = 1] = "Green";
     Color1[Color1["Blue"] = 2] = "Blue";
 })(Color1 || (Color1 = {}));
-var c1 = Color1.Red;
+let c1 = Color1.Red;
 // - 기본적으로 enum의 인덱스는 0부터 시작
 // - 인덱스를 임의로 설정할 수 있음
 var Color2;
@@ -48,14 +49,14 @@ var Color2;
     Color2[Color2["Green"] = 2] = "Green";
     Color2[Color2["Blue"] = 3] = "Blue";
 })(Color2 || (Color2 = {}));
-var c2 = Color2.Green;
+let c2 = Color2.Green;
 var Color3;
 (function (Color3) {
     Color3[Color3["Red"] = 1] = "Red";
     Color3[Color3["Green"] = 2] = "Green";
     Color3[Color3["Blue"] = 4] = "Blue";
 })(Color3 || (Color3 = {}));
-var c3 = Color3.Blue;
+let c3 = Color3.Blue;
 // 인덱스 값으로 해당 enum 값을 얻을 수도 있음
 var Color4;
 (function (Color4) {
@@ -63,20 +64,20 @@ var Color4;
     Color4[Color4["Green"] = 3] = "Green";
     Color4[Color4["Blue"] = 4] = "Blue";
 })(Color4 || (Color4 = {}));
-var colorName = Color4[3];
+let colorName = Color4[3];
 console.log(colorName); // Green
 // 7) Any
 // - 알지 못하는 타입. 임의의 타입, 컴파일 통과
-var notSure1 = 4;
+let notSure1 = 4;
 notSure1 = "maybe a string instead";
 notSure1 = false;
-var notSure2 = 4;
+let notSure2 = 4;
 notSure2.ifItExist(); // 성공
 notSure2.toFixed(); // 성공
-var prettySure = 4;
+let prettySure = 4;
 // prettySure.toFixed(); // 오류. object에 toFixed 존재하지않음.
 // - 타입의 일부만 아는 경우에 유용 (여러 타입이 섞인 배열 등)
-var list3 = [1, true, "free"];
+let list3 = [1, true, "free"];
 list3[1] = "zz";
 // 8) Void
 // - Any의 반대 타입. 어떤 타입도 존재할 수 없음.
@@ -84,12 +85,12 @@ function warnUser() {
     console.log("This is my warning message");
 }
 // - Void 타입에는 null 혹은 undefined 만 할당 가능
-var unusable = undefined;
+let unusable = undefined;
 unusable = null; // `--strictNullChecks` 사용 안할 때만 성공
 // 9) Null and Undefined
 // - 각각의 타입이름도 값과 같은 null, undefined
-var u = undefined;
-var n = null;
+let u = undefined;
+let n = null;
 // - Null과 Undefined는 모든 타입의 하위 타입이다. => 다른 모든 타입에 할당 가능
 // - `--strictNullChecks`를 사용하면
 //      1) null 과 undefined 자신에게 할당 혹은 any에 할당 가능
@@ -122,8 +123,8 @@ create(null);
 // - angle-bracket 문법 / as 문법
 // - typescript와 JSX를 같이 사용시에는 as 문법만 허용
 // angle-bracket 문법
-var someValue1 = "this is a string";
-var strLength1 = someValue1.length;
+let someValue1 = "this is a string";
+let strLength1 = someValue1.length;
 // as 문법
-var someValue2 = "this is a string";
-var strLength2 = someValue1.length;
+let someValue2 = "this is a string";
+let strLength2 = someValue1.length;
--- src/20-12-16/3.Functions.js
+++ src/20-12-16/3.Functions.js
@@ -8,9 +8,9 @@ function add(x, y) {
     return x + y;
 }
 //   anonymous function
-var myAdd = function (x, y) { return x + y; };
+let myAdd = function (x, y) { return x + y; };
 // - 함수 외부의 변수 참조 가능
-var z = 100;
+let z = 100;
 function addToZ(x, y) {
     return x + y + z;
 }
@@ -22,13 +22,13 @@ function addToZ(x, y) {
 function add2(x, y) {
     return x + y;
 }
-var myAdd2 = function (x, y) { return x + y; };
+let myAdd2 = function (x, y) { return x + y; };
 // - 화살표 표기를 사용하여 표현하는 방식
-var myAdd3 = function (x, y) { return x + y; };
+let myAdd3 = function (x, y) { return x + y; };
 // 3) 타입의 추론
 // - 컴파일러는 한 쪽에만 타입이 있어도 타입을 알아낼 수 있다
-var myAdd4 = function (x, y) { return x + y; };
-var myAdd5 = function (x, y) { return x + y; };
+let myAdd4 = function (x, y) { return x + y; };
+let myAdd5 = function (x, y) { return x + y; };
 // 4) 선택적 매개변수 / 기본 매개변수
 // - [기본 매개변수]
 // - 함수가 호출될 때 각 매개변수에 값을 주었는지 검사
@@ -38,7 +38,7 @@ function buildName(firstName, lastName) {
 }
 // let result1 = buildName("Bob"); // 오류
 // let result2 = buildName("Bob", "Adams", "Sr."); // 오류
-var result3 = buildName("Bob", "Adams"); // 정확함
+let result3 = buildName("Bob", "Adams"); // 정확함
 // - [선택적 매개변수]
 // - 매개변수 이름 끝에 '?' 를 붙임
 function buildName2(firstName, lastName) {
@@ -47,41 +47,35 @@ function buildName2(firstName, lastName) {
     else
         return firstName;
 }
-var result1 = buildName2("Bob"); // 정확함
+let result1 = buildName2("Bob"); // 정확함
 // let result2 = buildName2("Bob", "Adams", "Sr."); // 오류
-var result4 = buildName2("Bob", "Adams"); // 정확함
+let result4 = buildName2("Bob", "Adams"); // 정확함
 // - 기본-초기화 매개변수
 // - 값이 제공되지 않았거나 undefined 일 때 할당됨
-function buildName3(firstName, lastName) {
-    if (lastName === void 0) { lastName = "Smith"; }
+function buildName3(firstName, lastName = "Smith") {
     return firstName + " " + lastName;
 }
-var result5 = buildName3("Bob");
-var result6 = buildName3("Bob", undefined); // Bob Smith 리턴
+let result5 = buildName3("Bob");
+let result6 = buildName3("Bob", undefined); // Bob Smith 리턴
 // let result7 = buildName3("Bob", "Adams", "Sr."); // 오류
-var result8 = buildName3("Bob", "Adams"); // 정확함
+let result8 = buildName3("Bob", "Adams"); // 정확함
 // - 선택적 매개변수와 기본-초기화 매개변수가 필수 매개변수의 뒤에 올 때는 생략하면 알아서 처리됨
 // - 그 반대의 경우에는 undefined를 명시적으로 전달해야 함
-function buildName4(firstName, lastName) {
-    if (firstName === void 0) { firstName = "Will"; }
+function buildName4(firstName = "Will", lastName) {
     return firstName + " " + lastName;
 }
 // let result8 = buildName4("Bob"); // 오류
 // let result9 = buildName4("Bob", "Adams", "Sr."); // 오류
-var result10 = buildName4("Bob", "Adams"); // Bob Adams 리턴
-var result11 = buildName4(undefined, "Adams"); // Will Adams 리턴
+let result10 = buildName4("Bob", "Adams"); // Bob Adams 리턴
+let result11 = buildName4(undefined, "Adams"); // Will Adams 리턴
 // 5) 나머지(Rest) 매개변수
 // - 한 번에 하나의 매개변수가 아닌 다수의 매개변수를 그룹지을 때
 // - 자바스크립트의 경우에는 arguments 변수 사용
 // - 타입스크립트에서는 매개변수의 이름 앞에 '...'를 붙임
-function buildName5(firstName) {
-    var restOfName = [];
-    for (var _i = 1; _i < arguments.length; _i++) {
-        restOfName[_i - 1] = arguments[_i];
-    }
+function buildName5(firstName, ...restOfName) {
     return firstName + " " + restOfName.join(" ");
 }
-var employeeName = buildName5("Joseph", "Samuel", "Lucas", "MacKinzie");
+let employeeName = buildName5("Joseph", "Samuel", "Lucas", "MacKinzie");
 // 6) this
 // - this / 화살표 함수
 // - this는 함수가 호출될 때 정해지는 변수
@@ -102,20 +96,19 @@ var employeeName = buildName5("Joseph", "Samuel", "Lucas", "MacKinzie");
 // let pickedCard = cardPicker();
 // alert("card: " + pickedCard.card + " of " + pickedCard.suit); // 오류
 // 화살표 함수를 사용한 예제
-var deck = {
+let deck = {
     suits: ["hearts", "spades", "clubs", "diamonds"],
     cards: Array(52),
     createCardPicker: function () {
-        var _this = this;
-        return function () {
-            var pickedCard = Math.floor(Math.random() * 52);
-            var pickedSuit = Math.floor(pickedCard / 13);
-            return { suit: _this.suits[pickedSuit], card: pickedCard % 13 };
+        return () => {
+            let pickedCard = Math.floor(Math.random() * 52);
+            let pickedSuit = Math.floor(pickedCard / 13);
+            return { suit: this.suits[pickedSuit], card: pickedCard % 13 };
         };
     }
 };
-var cardPicker = deck.createCardPicker();
-var pickedCard = cardPicker();
+let cardPicker = deck.createCardPicker();
+let pickedCard = cardPicker();
 alert("card: " + pickedCard.card + " of " + pickedCard.suit);
 // 7) this 매개변수
 // - 명시적으로 this를 정하고자 할 때 사용
@@ -123,20 +116,19 @@ alert("card: " + pickedCard.card + " of " + pickedCard.suit);
 function f() {
     // 독립형 함수에서 `this`를 사용할 수 없다.
 }
-var deck2 = {
+let deck2 = {
     suits: ["hearts", "spades", "clubs", "diamonds"],
     cards: Array(52),
     createCardPicker: function () {
-        var _this = this;
-        return function () {
-            var pickedCard = Math.floor(Math.random() * 52);
-            var pickedSuit = Math.floor(pickedCard / 13);
-            return { suit: _this.suits[pickedSuit], card: pickedCard % 13 };
+        return () => {
+            let pickedCard = Math.floor(Math.random() * 52);
+            let pickedSuit = Math.floor(pickedCard / 13);
+            return { suit: this.suits[pickedSuit], card: pickedCard % 13 };
         };
     }
 };
-var cardPicker2 = deck.createCardPicker();
-var pickedCard2 = cardPicker();
+let cardPicker2 = deck.createCardPicker();
+let pickedCard2 = cardPicker();
 alert("card: " + pickedCard2.card + " of " + pickedCard2.suit);
 // 잘못된 예제
 // class Handler {
@@ -148,55 +140,52 @@ alert("card: " + pickedCard2.card + " of " + pickedCard2.suit);
 // let h = new Handler();
 // uiElement.addClickListener(h.onClickBad); // 오류
 // 올바른 예제 (1)
-var Handler = /** @class */ (function () {
-    function Handler() {
-    }
-    Handler.prototype.onClickGood = function (e) {
+class Handler {
+    onClickGood(e) {
         console.log('clicked!');
-    };
-    return Handler;
-}());
+    }
+}
 // 올바른 예제 (2)
 // class Handler {
 //     info : string;
 //     onClickGood = (e : Event) => { this.info = e.message }
 // }
-var h = new Handler();
+let h = new Handler();
 // uiElement.addClickListener(h.onClickGood);
 // 9) 오버로드
 // - 전달된 인자의 형태에 따라 다른 리턴 방식을 정해줄 때
 // - [javascript] 방식
-var suits = ["hearts", "spades", "clubs", "diamonds"];
+let suits = ["hearts", "spades", "clubs", "diamonds"];
 function pickCard(x) {
     if (typeof x == "object") {
-        var pickedCard_1 = Math.floor(Math.random() * x.length);
-        return pickedCard_1;
+        let pickedCard = Math.floor(Math.random() * x.length);
+        return pickedCard;
     }
     else if (typeof x == "number") {
-        var pickedSuit = Math.floor(x / 13);
+        let pickedSuit = Math.floor(x / 13);
         return { suit: suits[pickedSuit], card: x % 13 };
     }
 }
-var myDeck = [{ suit: "diamonds", card: 2 }, { suit: "spades", card: 10 }, { suit: "hearts", card: 4 }];
-var pickedCard3 = myDeck[pickCard(myDeck)];
+let myDeck = [{ suit: "diamonds", card: 2 }, { suit: "spades", card: 10 }, { suit: "hearts", card: 4 }];
+let pickedCard3 = myDeck[pickCard(myDeck)];
 alert("card: " + pickedCard3.card + " of " + pickedCard3.suit);
-var pickedCard4 = pickCard(15);
+let pickedCard4 = pickCard(15);
 alert("card: " + pickedCard4.card + " of " + pickedCard4.suit);
 //----------------------------------------------------------------
 // - [typescript] 방식
-var suits2 = ["hearts", "spades", "clubs", "diamonds"];
+let suits2 = ["hearts", "spades", "clubs", "diamonds"];
 function pickCard2(x) {
     if (typeof x == "object") {
-        var pickedCard_2 = Math.floor(Math.random() * x.length);
-        return pickedCard_2;
+        let pickedCard = Math.floor(Math.random() * x.length);
+        return pickedCard;
     }
     else if (typeof x == "number") {
-        var pickedSuit = Math.floor(x / 13);
+        let pickedSuit = Math.floor(x / 13);
         return { suit: suits2[pickedSuit], card: x % 13 };
     }
 }
-var myDeck2 = [{ suit: "diamonds", card: 2 }, { suit: "spades", card: 10 }, { suit: "hearts", card: 4 }];
-var pickedCard5 = myDeck2[pickCard2(myDeck2)];
+let myDeck2 = [{ suit: "diamonds", card: 2 }, { suit: "spades", card: 10 }, { suit: "hearts", card: 4 }];
+let pickedCard5 = myDeck2[pickCard2(myDeck2)];
 alert("card: " + pickedCard5.card + " of " + pickedCard5.suit);
-var pickedCard6 = pickCard(15);
+let pickedCard6 = pickCard(15);
 alert("card: " + pickedCard6.card + " of " + pickedCard6.suit);
