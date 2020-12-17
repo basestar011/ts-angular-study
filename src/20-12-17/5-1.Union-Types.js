d790d1968806a1db6cc040af8758be484c02fe79
dongjun.han <dongjun.han@owaysoft.com>
2020-12-17 17:46:42 +0900

    20-12-17

--- src/20-12-17/5-1.Union-Types.js
+++ src/20-12-17/5-1.Union-Types.js
@@ -13,19 +13,19 @@ function padLeft(value, padding) {
     if (typeof padding === "string") {
         return padding + value;
     }
-    throw new Error("Expected string or number, got '" + padding + "'");
+    throw new Error(`Expected string or number, got '${padding}'`);
 }
 padLeft("Hello world", 4);
 padLeft("Hello world", "Hi ");
 padLeft("Hello world", {});
 // -- 생략
 // 컴파일에서는 통과하지만 런타임 시 오류 발생
-var indentedString = padLeft2("Hello World", true);
+let indentedString = padLeft2("Hello World", true);
 // padding 타입을 any 대신에 유니언 타입을 사용
 function padLeft3(value, padding) {
     // ...
 }
-var pet = getSmallPet();
+let pet = getSmallPet();
 pet.layEggs();
 function networkStatus(state) {
     // 모든 타입의 공통이 아닌 프로퍼티에 접근하려는 시도는 오류를 발생 시킨다.
@@ -34,8 +34,8 @@ function networkStatus(state) {
         case "loading":
             return "Downloading...";
         case "failed":
-            return "Error " + state.code + " downloading";
+            return `Error ${state.code} downloading`;
         case "success":
-            return "Downloaded " + state.response.title + " - " + state.response.summary;
+            return `Downloaded ${state.response.title} - ${state.response.summary}`;
     }
 }
