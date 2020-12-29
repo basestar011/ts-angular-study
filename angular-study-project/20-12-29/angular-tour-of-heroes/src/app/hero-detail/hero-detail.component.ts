2d06bf221d88b1b5461e0d0c3c9fe48bfb17f623
dongjun.han <dongjun.han@owaysoft.com>
2020-12-29 18:11:34 +0900

    20-12-29

---
+++ angular-study-project/20-12-29/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,42 @@
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from '../hero.service';
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+
+export class HeroDetailComponent implements OnInit {
+  hero : Hero;
+
+  constructor(
+    private route : ActivatedRoute,
+    private heroService : HeroService,
+    private location : Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero() : void {
+    const id = +this.route.snapshot.paramMap.get('id'); // + 연산자는 string을 number로 변환
+    this.heroService.getHero(id)
+        .subscribe(hero => this.hero = hero);
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
+
+  goBack() : void {
+    this.location.back();
+  }
+
+}
